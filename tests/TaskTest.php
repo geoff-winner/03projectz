83d0af6fee93f5658bdaf504e039b676b0483c02
geoff winner & james nielson <student@epicodus.com>
2015-03-16 10:19:23 -0700

    allow us to test

---
+++ tests/TaskTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+*@backupGlobals disabled
+*@backupStaticAttributes disabled
+*/
+
+    require_once "src/Task.php";
+
+    $DB = new PDO('pgsql:host=localhost;dbname=dbname=to_do_test');
+
+    class TaskTest extends PHPUnit_Framework_TestCase
+    {
+
+        function test_save()
+        {
+            //Arrange
+            $description = "Wash the dog";
+            $test_task = new Task($description);
+
+            //Act
+            $test_task->save();
+
+            //Assert
+            $result = Task::getAll();
+            $this->assertEquals($test_task, $result[0]);
+
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $description = "Wash the dog";
+            $description2 = "Water the lawn";
+            $test_Task = new Task($description);
+            $test_Task->save();
+            $test_Task2 = new Task($description2);
+            $test_Task2->save();
+
+            //Act
+            $result = Task::getAll();
+
+            //Assert
+            $this->assertEquals([$test_Task $test_Task2], $result);
+        }
+    }
+
+
+?>
